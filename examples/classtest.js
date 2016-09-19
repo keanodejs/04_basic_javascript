f9857c635fc19da9768d19ae2a8388286d2ab5b3
Claus Bové <clbo@kea.dk>
2016-09-19 22:58:25 +0200

    classtest.js added some code

--- examples/classtest.js
+++ examples/classtest.js
@@ -6,6 +6,14 @@ class Polygon {
         this.height = height;
         this.width = width;
         this.test = arguments[2];
+        //this._name = 'Claus';
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(newName){
+        this._name = newName;
     }
 
     get area() {
@@ -22,3 +30,5 @@ const square = new Polygon(10, 10, 30);
 console.log(square.area);
 square.height = 300;
 console.log(square.test);
+square.name = 'hanibal';
+console.log(square.name);
---
+++ exercises/06_Exercise_module_pattern.js
@@ -0,0 +1,9 @@
+// By using closure and the Module pattern
+// Create an object called person
+// It should have a name, lastName, cpr, phoneNumber and email.
+// they should all be private members
+
+// return an object with getters and setters to the private variables
+
+
+
